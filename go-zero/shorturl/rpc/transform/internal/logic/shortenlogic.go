aec1d6abf640e9f5e29ee2b2f993ec962be0f2bb
vagrant <vagrant@node1>
2023-05-04 08:24:56 +0000

    feat: 修改 shorten/expand rpc 代码调用 crud+cache 代码

--- go-zero/shorturl/rpc/transform/internal/logic/shortenlogic.go
+++ go-zero/shorturl/rpc/transform/internal/logic/shortenlogic.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"github.com/zeromicro/go-zero/core/hash"
+	"shorturl/rpc/transform/model"
 
 	"shorturl/rpc/transform/internal/svc"
 	"shorturl/rpc/transform/transform"
@@ -25,6 +27,18 @@ func NewShortenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShortenLo
 
 func (l *ShortenLogic) Shorten(in *transform.ShortenReq) (*transform.ShortenResp, error) {
 	// todo: add your logic here and delete this line
+	// 手动代码开始，生成短链接
+	key := hash.Md5Hex([]byte(in.Url))[:6]
+	_, err := l.svcCtx.Model.Insert(l.ctx, &model.Shorturl{
+		Shorten: key,
+		Url:     in.Url,
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	return &transform.ShortenResp{}, nil
+	return &transform.ShortenResp{
+		Shorten: key,
+	}, nil
+	// 手动代码结束
 }
