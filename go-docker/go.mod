2340a44c8d8f8be29447e6a47c63827dd1d1f24f
vagrant <vagrant@node2>
2023-05-30 05:56:09 +0000

    feat: go docker example

---
+++ go-docker/go.mod
@@ -0,0 +1,8 @@
+module go-docker
+
+go 1.19
+
+require (
+	github.com/gorilla/mux v1.8.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
---
+++ go-docker/hello_server.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+	log.Printf("Received request for %s\n", name)
+	w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
+}
+
+func main() {
+	// Create Server and Route Handlers
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handler)
+	r.HandleFunc("/health", Health(""))
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Configure Logging
+	LOG_FILE_LOCATION := os.Getenv("LOG_FILE_LOCATION")
+	if LOG_FILE_LOCATION != "" {
+		log.SetOutput(&lumberjack.Logger{
+			Filename:   LOG_FILE_LOCATION,
+			MaxSize:    500, // megabytes
+			MaxBackups: 3,
+			MaxAge:     28,   //days
+			Compress:   true, // disabled by default
+		})
+	}
+
+	// Start Server
+	go func() {
+		log.Println("Starting Server")
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Graceful Shutdown
+	waitForShutdown(srv)
+}
+
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down")
+	os.Exit(0)
+}
+
+const (
+	contentType     = "Content-Type"
+	contentTypeJSON = "application/health+json"
+	svcStatus       = "pass"
+	description     = " service"
+)
+
+var (
+	// Version represents the last service git tag in git history.
+	// It's meant to be set using go build ldflags:
+	// -ldflags "-X 'main.Version=0.0.0'"
+	Version = "0.0.0"
+	// Commit represents the service git commit hash.
+	// It's meant to be set using go build ldflags:
+	// -ldflags "-X 'main.Commit=ffffffff'"
+	Commit = "ffffffff"
+	// BuildTime represetns the service build time.
+	// It's meant to be set using go build ldflags:
+	// -ldflags "-X 'main.BuildTime=1970-01-01_00:00:00'"
+	BuildTime = "1970-01-01_00:00:00"
+)
+
+// HealthInfo contains version endpoint response.
+type HealthInfo struct {
+	// Status contains service status.
+	Status string `json:"status"`
+
+	// Version contains current service version.
+	Version string `json:"version"`
+
+	// Commit represents the git hash commit.
+	Commit string `json:"commit"`
+
+	// Description contains service description.
+	Description string `json:"description"`
+
+	// BuildTime contains service build time.
+	BuildTime string `json:"build_time"`
+}
+
+// Health exposes an HTTP handler for retrieving service health.
+func Health(service string) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add(contentType, contentTypeJSON)
+		if r.Method != http.MethodGet && r.Method != http.MethodHead {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		res := HealthInfo{
+			Status:      svcStatus,
+			Version:     Version,
+			Commit:      Commit,
+			Description: service + description,
+			BuildTime:   BuildTime,
+		}
+
+		w.WriteHeader(http.StatusOK)
+
+		if err := json.NewEncoder(w).Encode(res); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+}
