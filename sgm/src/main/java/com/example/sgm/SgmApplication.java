e1a518afbada7d5f2b7d5faad4f19a1b5422d317
liugaohua <lgh.eng@qq.com>
2023-07-19 13:34:02 +0800

    scheduled

--- sgm/src/main/java/com/example/sgm/SgmApplication.java
+++ sgm/src/main/java/com/example/sgm/SgmApplication.java
@@ -3,12 +3,14 @@ package com.example.sgm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @EnableTransactionManagement
 @SpringBootApplication
 @MapperScan("com.example.sgm.mapper")
+@EnableScheduling
 public class SgmApplication {
 
     public static void main(String[] args) {
