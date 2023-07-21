15a1b67e161ed34ddbec350e718a596c40ba7241
liugaohua <lgh.eng@qq.com>
2023-07-21 14:14:01 +0800

    spring boot admin

--- sgm/src/main/java/com/example/sgm/SgmApplication.java
+++ sgm/src/main/java/com/example/sgm/SgmApplication.java
@@ -3,12 +3,13 @@ package com.example.sgm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
+//
+//
+//@EnableAdminServer
 @EnableTransactionManagement
 @SpringBootApplication
 @MapperScan("com.example.sgm.mapper")
