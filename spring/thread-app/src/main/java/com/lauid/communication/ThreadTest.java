9c7bb6c0915fe391d93ada749e32f52f2301f5f5
gaohua.liu <lgh.eng@qq.com>
2023-08-09 21:57:55 +0800

    feat: 线程通信

---
+++ spring/thread-app/src/main/java/com/lauid/communication/ThreadTest.java
@@ -0,0 +1,38 @@
+package com.lauid.communication;
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        Desk desk = new Desk();
+
+        new Thread(() -> {
+            while (true){
+                desk.put();
+            }
+        }, "厨师1").start();
+
+        new Thread(() -> {
+            while (true){
+                desk.put();
+            }
+
+        }, "厨师2").start();
+
+        new Thread(() -> {
+            while (true){
+                desk.put();
+            }
+        }, "厨师3").start();
+
+
+        new Thread(() -> {
+            while (true){
+                desk.get();
+            }
+        }, "吃货1").start();
+        new Thread(() -> {
+            while (true){
+                desk.get();
+            }
+        }, "吃货2").start();
+    }
+}
