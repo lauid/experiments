bfbd0d7e946308c16fe7d917c78189a851fa1e11
liugaohua <lgh.eng@qq.com>
2023-09-13 09:27:57 +0800

    feat: wire 依赖注入

---
+++ demo/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main()  {
+	str := `{"req":{"SoftwareCode":"aa","HardwareCode":"ZK100A7512M512M202208005448","MAC":"fa:1d:73:57:3b:b8"}}`
+	fmt.Println(len(str))
+}
